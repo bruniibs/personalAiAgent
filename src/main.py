1add79b49b7e9fa0a7a3bdc6d47ca4b45f577f21
boo <studiiesboo@gmail.com>
2026-09-23 19:41:07 -0300

    feat: created initial simple code

--- src/main.py
+++ src/main.py
@@ -1,1 +1,14 @@
-## fazer um programa que mostre mensagem
+
+print("Cognitive Assistant System starting...")
+
+# CAS asks for your name => how she should adress you. When the name is received she will exclude any empty space, if the name is empty it'll use Miss instead.
+name = input("What is your name? ").strip()
+if not name:
+    name = "Miss"
+print(f"Hello, {name}!\nHow could I assist you today?")
+
+while True:
+    user_input = input("> ")
+    if user_input.lower() in ["exit", "quit"]:
+        print("Goodbye! Have a great day!")
+        break
